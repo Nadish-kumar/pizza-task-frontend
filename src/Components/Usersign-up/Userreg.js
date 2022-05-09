fbf2fa1444d1f9fb1bd99f1affa102735f2708b3
@kumarnadi22 <kumarnadi22@gmail.com>
2022-05-10 00:33:46 +0530

    ref sign up page

--- src/Components/Usersign-up/Userreg.js
+++ src/Components/Usersign-up/Userreg.js
@@ -3,6 +3,7 @@ import './Userreg.css';
 import { useFormik } from 'formik';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import record from "../../assets/img/pizza.jpg"
 
 const Userreg = () => {
 
@@ -32,96 +33,64 @@ const Userreg = () => {
 
 
     return (
-        <div className='container singup'>
-        <div className='row'>
-
-     
-      <section class="vh-100" >
-      <div class="container h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-          <div class="col-lg-12 col-xl-11">
-            <div class="card text-black" >
-              <div class="card-body p-md-5">
-                <div class="row justify-content-center">
-                  <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-    
-                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
-    
-                    <form class="mx-1 mx-md-4" onSubmit={formik.handleSubmit}>
-    
-                      <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                          <input type="text" id="form3Example1c" class="form-control"  name="name"
-                            onChange={formik.handleChange}
-                            value={formik.values.name} />
-                          <label class="form-label" for="form3Example1c">Your Name</label>
-                        </div>
-                      </div>
-    
-                      <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                          <input type="email" id="form3Example3c" class="form-control"  name="email"
-                            onChange={formik.handleChange}
-                            value={formik.values.email} />
-                          <label class="form-label" for="form3Example3c">Your Email</label>
-                        </div>
-                      </div>
-    
-                      <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                          <input type="password" id="form3Example4c" class="form-control"  name="password"
-                            onChange={formik.handleChange}
-                            value={formik.values.password} />
-                          <label class="form-label" for="form3Example4c">Password</label>
-                        </div>
-                      </div>
-    
-                      <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-key fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                          <input type="number" id="form3Example4cd" class="form-control"  name="phone"
-                            onChange={formik.handleChange}
-                            value={formik.values.phone}/>
-                          <label class="form-label" for="form3Example4cd">Mobile Number</label>
-                        </div>
-                      </div>
-    
-                      <div class="form-check d-flex justify-content-center mb-5">
-                        <input
-                          class="form-check-input me-2"
-                          type="checkbox"
-                          value=""
-                          id="form2Example3c"
-                        />
-                        <label class="form-check-label" for="form2Example3">
-                          I agree all statements in <a href="#!">Terms of service</a>
-                        </label>
-                      </div>
-    
-                      <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                        <input type="submit" class="btn btn-primary btn-lg" />
-                      </div>
-    
-                    </form>
-    
-                  </div>
-                  <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-    
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp" class="img-fluid" alt="Sample image" />
-    
-                  </div>
+      <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-6">
+          <img className="gmail1" src={record} />
+        </div>
+        <div className="col-md-6">
+          {/* <img className="gamil2" src={} /> */}
+          <p className="heading">Register</p>
+          <form onSubmit={formik.handleSubmit}>
+            <div className="row">
+              <div className="col-md-2"></div>
+              <div className="col-md-8">
+                <input
+                  type="text"
+                  placeholder="Enter your first Name"
+                  className="form-control inputfield1"
+                  onChange={formik.handleChange}
+                  value={formik.values.name}
+                  name="name"
+                />
+               
+                <input
+                  type="email"
+                  placeholder="Enter your mail"
+                  className="form-control inputfield1"
+                  onChange={formik.handleChange}
+                  value={formik.values.email}
+                  name="email"
+                />
+                <input
+                  type="password"
+                  placeholder="Enter your password"
+                  className="form-control inputfield1"
+                  onChange={formik.handleChange}
+                  value={formik.values.password}
+                  name="password"
+                />
+                <input
+                  type="number"
+                  placeholder="Enter your Number"
+                  className="form-control inputfield1"
+                  onChange={formik.handleChange}
+                  value={formik.values.phone}
+                  name="phone"
+                />
+               
+                <div className="buttongro1">
+                  <button className="btn btn-primary button" type="submit">
+                    Register
+                  </button>
                 </div>
               </div>
+              <div className="col-md-2"></div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
-    </section>
     </div>
-      </div>  
     )
 }
 
